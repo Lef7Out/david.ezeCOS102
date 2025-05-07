c1f06608200a450e5fa0396659a099b9c331144d
Lef7Out <daveze727@gmail.com>
2025-05-07 01:51:43 +0100

    Project 1 and Practices Complete

---
+++ week3/classwork.py
@@ -0,0 +1,20 @@
+
+
+# radius = float(input("Enter the radius of the circle: "))
+
+# def calculate_area(radius):
+#     pi = 3.14
+#     area = pi * radius ** 2
+#     return area
+
+# print("The area of the circle is", calculate_area(radius))
+
+input1 = input("Enter a string: ")
+def is_palindrome():
+    if input1 == input1[::-1]:
+        print("TRUE")
+    else:
+        print("FALSE")
+is_palindrome()
+
+    
